beb5dcee0027363256899b77b034d9f06663b3e3
DeshawnThompson <djzz2723@gmail.com>
2025-03-19 20:23:31 -0400

    Update calc_one.py

--- calc_one.py
+++ calc_one.py
@@ -1,12 +1,11 @@
 
 def add(number1,number2):
-    print ("your number", number1, "plus your",number2, "is")
-    print (number1 + number2)
+    return (number1 + number2)
 def sub(number1,number2):
-    print (number1 - number2)
+    return (number1 - number2)
 def divide(number1,number2):
-    print (number1 / number2)
+    return (number1 / number2)
 def multiply(number1,number2):
-    print (number1 * number2)
+    return (number1 * number2)
 def mod (number1, number2):
-    print(number1 % number2)
+    return(number1 % number2)
